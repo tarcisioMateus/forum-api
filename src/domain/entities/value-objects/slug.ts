e235c332e87edc5a64fcff17385e43b99e20caea
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2025-06-22 22:47:47 -0300

    basic structure started

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,27 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+/**
+ * Receives a string and normalize it as a slug.
+ *
+ * @example "An example title" => "an-example-title"
+ *
+ * @param text {string}
+ */
+static createFromText(text: string) {
+  const slugText = text
+    .normalize("NFD")
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/-$/g, '')
+    .replace(/^-/g, '')
+
+  return new Slug(slugText)
+}
+}
