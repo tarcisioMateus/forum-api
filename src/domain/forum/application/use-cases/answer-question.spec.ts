681649cb575ef9a83a314f5241071b77a747ce4f
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2025-07-01 09:53:35 -0300

    created test/factories

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,5 +1,5 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import { InMemoryAnswersRepository } from 'test/in/in-memory-answers-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 let sut: AnswerQuestionUseCase
