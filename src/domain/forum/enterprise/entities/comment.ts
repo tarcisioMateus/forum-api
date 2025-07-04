06114fd4543878352010edbac384ae221140cbdb
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2025-07-04 07:40:24 -0300

    enteties/comment

---
+++ src/domain/forum/enterprise/entities/comment.ts
@@ -0,0 +1,42 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export interface CommentProps {
+  authorId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export abstract class Comment<
+  Props extends CommentProps,
+> extends Entity<Props> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 60).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+}
--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,47 +1,14 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { CommentProps, Comment } from './comment'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-
+export class QuestionComment extends Comment<QuestionCommentProps> {
   get questionId() {
     return this.props.questionId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  get excerpt() {
-    return this.content.substring(0, 60).trimEnd().concat('...')
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
   }
 
   static create(
