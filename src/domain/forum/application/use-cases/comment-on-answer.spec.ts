a8c4ff103c8a7bdc4b6b06fef9479834225ec624
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2025-07-04 08:38:21 -0300

    use-cases/comment-on-answer

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,33 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to create an answer comment', async () => {
+    const answer = makeAnswer()
+
+    inMemoryAnswersRepository.create(answer)
+
+    const { answerComment } = await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: '1',
+      content: 'New comment',
+    })
+
+    expect(answerComment.content).toEqual('New comment')
+  })
+})
