0fe7f1c3d86f1c41cc7fd9e929520a4ad12030f1
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2025-07-06 08:37:47 -0300

    use-cases/delete-answer -- attachments

--- src/domain/forum/application/use-cases/delete-answer.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -4,6 +4,8 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { DeleteAnswerUseCase } from './delete-answer'
 import { NotAllowedError } from './errors/not-allowed-error'
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachment-repository'
+import { makeAnswerAttachment } from 'test/factories/make-answer-attachment'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
@@ -59,5 +61,40 @@ describe('Delete Answer', () => {
     expect(response.isLeft()).toBeTruthy()
     expect(response.value).toBeInstanceOf(NotAllowedError)
     expect(inMemoryAnswersRepository.items).toHaveLength(1)
+  })
+
+  it('should be able to delete an answer with its attachments', async () => {
+    const answer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    const attachments = [
+      makeAnswerAttachment({
+        answerId: new UniqueEntityID('answer-1'),
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      makeAnswerAttachment({
+        answerId: new UniqueEntityID('answer-1'),
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    ]
+
+    answer.attachments = new AnswerAttachmentList(attachments)
+    await inMemoryAnswersRepository.create(answer)
+
+    expect(inMemoryAnswerAttachmentsRepository.items).toHaveLength(2)
+
+    const response = await sut.execute({
+      authorId: 'author-1',
+      answerId: 'answer-1',
+    })
+
+    expect(response.isRight()).toBeTruthy()
+    expect(response.value).toEqual(null)
+    expect(inMemoryAnswersRepository.items).toHaveLength(0)
+    expect(inMemoryAnswerAttachmentsRepository.items).toHaveLength(0)
   })
 })
