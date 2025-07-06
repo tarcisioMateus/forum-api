fb1b1513e357dc5c3d0c726eefb9cfa1a1d9e8b2
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2025-07-06 08:32:58 -0300

    use-cases/edit-answer -- attachments

--- src/domain/forum/application/use-cases/edit-answer.spec.ts
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -4,6 +4,8 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { EditAnswerUseCase } from './edit-answer'
 import { NotAllowedError } from './errors/not-allowed-error'
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachment-repository'
+import { makeAnswerAttachment } from 'test/factories/make-answer-attachment'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
@@ -17,7 +19,10 @@ describe('Edit Answer', () => {
       inMemoryAnswerAttachmentsRepository,
     )
 
-    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+    sut = new EditAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerAttachmentsRepository,
+    )
   })
 
   it('should be able to edit an answer', async () => {
@@ -34,6 +39,7 @@ describe('Edit Answer', () => {
       authorId: 'author-1',
       answerId: 'answer-1',
       content: 'new content',
+      attachmentsId: [],
     })
 
     expect(response.isRight()).toBeTruthy()
@@ -57,10 +63,53 @@ describe('Edit Answer', () => {
       authorId: 'author-2',
       answerId: 'answer-1',
       content: 'new content',
+      attachmentsId: [],
     })
 
     expect(response.isLeft()).toBeTruthy()
     expect(response.value).toBeInstanceOf(NotAllowedError)
     expect(inMemoryAnswersRepository.items).toHaveLength(1)
+  })
+
+  it('should be able to edit an answer with its attachments', async () => {
+    const answer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    const attachments = [
+      makeAnswerAttachment({
+        answerId: new UniqueEntityID('answer-1'),
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      makeAnswerAttachment({
+        answerId: new UniqueEntityID('answer-1'),
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    ]
+
+    answer.attachments = new AnswerAttachmentList(attachments)
+    await inMemoryAnswersRepository.create(answer)
+
+    const response = await sut.execute({
+      authorId: 'author-1',
+      answerId: 'answer-1',
+      content: 'new content',
+      attachmentsId: ['2', '3', '4'],
+    })
+
+    expect(response.isRight()).toBeTruthy()
+    expect(response.value).toEqual(null)
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: 'new content',
+    })
+    expect(inMemoryAnswerAttachmentsRepository.items).toHaveLength(3)
+    expect(inMemoryAnswerAttachmentsRepository.items).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('3') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('4') }),
+    ])
   })
 })
--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -2,11 +2,16 @@ import { AnswersRepository } from '../repositories/answers-repository'
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 import { NotAllowedError } from './errors/not-allowed-error'
+import { AnswerAttachmentsRepository } from '../repositories/answer-attachments-repository'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
 
 interface EditAnswerUseCaseRequest {
   answerId: string
   authorId: string
   content: string
+  attachmentsId: string[]
 }
 
 type EditAnswerUseCaseResponse = Either<
@@ -15,12 +20,16 @@ type EditAnswerUseCaseResponse = Either<
 >
 
 export class EditAnswerUseCase {
-  constructor(private answersRepository: AnswersRepository) {}
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answerAttachmentsRepository: AnswerAttachmentsRepository,
+  ) {}
 
   async execute({
     answerId,
     authorId,
     content,
+    attachmentsId,
   }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
     const answer = await this.answersRepository.findById(answerId)
 
@@ -32,6 +41,18 @@ export class EditAnswerUseCase {
       return left(new NotAllowedError())
     }
 
+    const currentAttachments =
+      await this.answerAttachmentsRepository.findManyByAnswerId(answerId)
+    answer.attachments = new AnswerAttachmentList(currentAttachments)
+
+    const updatedAttachments = attachmentsId.map((attachmentId) =>
+      AnswerAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        answerId: answer.id,
+      }),
+    )
+
+    answer.attachments.update(updatedAttachments)
     answer.content = content
 
     await this.answersRepository.save(answer)
