37e5ae8beed3b103372110d242c8077e823c8382
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2025-07-07 23:01:15 -0300

    forum/events/answer-created-event -- notification/subscribes/on-answer-created

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
@@ -34,6 +35,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
   async create(answer: Answer) {
     this.items.push(answer)
     this.answerAttachmentsRepository.create(answer.attachments.getItems())
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async save(answer: Answer): Promise<void> {
